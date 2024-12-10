bfd8f3e4a841318e200e594e161f07faa83991c1
Vojtus <diminutive.diminutive@gmail.com>
2024-12-10 08:47:58 +0100

    Add of show code and coloring

--- examples/message.py
+++ examples/message.py
@@ -2,6 +2,7 @@ import threading
 import time
 from queue import Queue
 
+from color import Color
 from examples.simulation import Simulation
 
 
@@ -29,3 +30,30 @@ class Messages(Simulation):
         producer_thread.join()
         consumer_thread.do_run = False
         consumer_thread.join()
+
+    def show_code(self):
+        print(f"""
+        {Color.GREEN}# Simulation of messaging{Color.RESET}
+                def producer(queue):
+            for i in range(5):
+                queue.put("Zpráva" + str(i))
+                print("Producent odeslal: Zpráva " + str(i))
+                time.sleep(1)
+
+        def consumer(queue):
+            while not queue.empty() or threading.current_thread().is_alive():
+                if not queue.empty():
+                    msg = queue.get()
+                    print(f"Konzument přijal: "+ str(msg))
+
+        queue = Queue()
+        producer_thread = threading.Thread(target=producer, args=(queue,))
+        consumer_thread = threading.Thread(target=consumer, args=(queue,))
+        consumer_thread.do_run = True
+
+        producer_thread.start()
+        consumer_thread.start()
+        producer_thread.join()
+        consumer_thread.do_run = False
+        consumer_thread.join()
+        """)
--- examples/simulation.py
+++ examples/simulation.py
@@ -1,4 +1,7 @@
-class Simulation:
+from abc import ABC
+
+
+class Simulation(ABC):
     def run(self):
         raise NotImplemented
     def show_code(self):
